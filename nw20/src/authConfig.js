4e60b3ce29fa850dbda5b167a9bddcb153d64e02
Mark Burgess <mburgess1983@gmail.com>
2025-07-23 12:26:40 +0100

    update authConfig.js

--- nw20/src/authConfig.js
+++ nw20/src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "6e8ac70e-90b1-426e-ada3-c1fa40aa0b6d",
         authority: "https://login.microsoftonline.com/6d52e229-7b09-4772-885e-edd85950a304",
-        redirectUri: "http://localhost:61536/",
+        redirectUri: "https://lemon-stone-02b13151e.2.azurestaticapps.net/",
     },
     cache: {
         cacheLocation: "localStorage",
